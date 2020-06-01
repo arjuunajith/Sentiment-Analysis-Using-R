5333f16c504ef7edf4b5c7259b58cd5b8e9ded99
Arjun Ajith <arjuunajith@gmail.com>
2020-06-01 23:05:09 +0530

    Plotting the words

--- AppleTweetAnalysis.R
+++ AppleTweetAnalysis.R
@@ -1,23 +1,23 @@
-#Reading File
+# Reading File
 apple <- read.csv(file.choose(), header =T)
 
-#Corpus
+# Corpus
 library(NLP) #for NLP package
 library(tm) #for tex mining
 corpus <- iconv(apple$text,  to='UTF-8', sub = "byte")
 corpus <- Corpus(VectorSource(corpus))
 inspect(corpus[1:5])
 
-#Data Cleaning
+# Data Cleaning starts
 corpus <- tm_map(corpus, tolower)
 corpus <- tm_map(corpus, removePunctuation)
 corpus <- tm_map(corpus, removeNumbers)
 
-#Specify Stopwords
+# Specify Stopwords
 cleanset <- tm_map(corpus, removeWords, stopwords('english'))
 inspect(cleanset[1:5])
 
-#Remove URLs
+# Remove URLs
 removeURL <- function(x) gsub('http[[:alnum:]]*', '', x)
 cleanset <- tm_map(cleanset, content_transformer(removeURL))
 inspect(cleanset[1:5])
@@ -36,4 +36,11 @@ tdm
 tdm <- as.matrix(tdm)
 tdm[1:10, 1:20]
 
+# Barplot
+w <- rowSums(tdm)
+w <- subset(w, w>=25)
+barplot(w,
+        las = 2,
+        col = "")
 
+#
